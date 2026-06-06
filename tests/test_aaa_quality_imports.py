eabd347261a55d11968be4fdb8dfadd71fcec47f
ZuluYokohama <b.jones@jtech.ai>
2026-06-06 09:16:31 -0500

    test(aaa): use sys.path.insert(0) to avoid importing a shadowing module

--- tests/test_aaa_quality_imports.py
+++ tests/test_aaa_quality_imports.py
@@ -2,7 +2,7 @@ import unittest
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../scripts')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../scripts')))
 import aaa_quality
 
 
